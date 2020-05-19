ba8e95f3f7a0bb63e449af218e22462360ad8079
qiaolei <qiaolei@fenrir-tec.com>
2020-05-19 15:17:49 +0800

    Simplify view data

--- app/src/main/java/xyz/do9core/lifegame/MainViewModel.kt
+++ app/src/main/java/xyz/do9core/lifegame/MainViewModel.kt
@@ -9,43 +9,40 @@ import xyz.do9core.game.Generation
 import xyz.do9core.game.Universe
 import xyz.do9core.game.createUniverse
 import xyz.do9core.game.life.randPoints
-import xyz.do9core.lifegame.view.BooleanMatrix
 
 class MainViewModel : ViewModel() {
 
-    private val universe: Universe = createUniverse(51, 41, time = 20) {
-        randPoints(0 to 0, 50 to 40, density = 0.1)
-    }
+    private val _universe = MutableLiveData<Universe>()
+    val width = Transformations.map(_universe) { it.width }
+    val height = Transformations.map(_universe) { it.height }
 
     private val _indexedGeneration = MutableLiveData<IndexedValue<Generation>>()
-    val generationCount = Transformations.map(_indexedGeneration) { it.index.toString() }
-    val dataMatrix = Transformations.map(_indexedGeneration) { indexedGeneration ->
-        val generation = indexedGeneration.value
-        BooleanMatrix(universe.width, universe.height).also { matrix ->
-            generation.lives.forEach { matrix[it.x, it.y] = true }
-        }
-    }
+    val generationCount = Transformations.map(_indexedGeneration) { it.index.inc().toString() }
+    val liveCells = Transformations.map(_indexedGeneration) { it.value.lives }
 
     private val _isActive = MutableLiveData(false)
     val isActive: LiveData<Boolean> = _isActive
 
     private var evolving: Job? = null
+
     fun bigBang() {
-        evolving?.cancel()
+        val universe = createUniverse(41, 41, time = 10) {
+            randPoints(0 to 0, 40 to 40, density = 0.1)
+        }
+        _universe.postValue(universe)
         evolving = universe.asFlow()
             .buffer()
             .flowOn(Dispatchers.Default)
             .onEach { delay(500) }
             .withIndex()
             .onEach { gen -> _indexedGeneration.postValue(gen) }
-            .onCompletion { _isActive.postValue(false) }
+            .onStart { _isActive.value = true }
+            .onCompletion { _isActive.value = false }
             .launchIn(viewModelScope)
-        _isActive.postValue(true)
     }
 
     fun heatDeath() {
         evolving?.cancel()
         evolving = null
-        _isActive.postValue(false)
     }
 }
--- app/src/main/java/xyz/do9core/lifegame/view/LifeGameView.kt
+++ app/src/main/java/xyz/do9core/lifegame/view/LifeGameView.kt
@@ -8,6 +8,7 @@ import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.withStyledAttributes
+import xyz.do9core.game.Position
 import xyz.do9core.lifegame.R
 import kotlin.math.min
 
@@ -18,23 +19,38 @@ class LifeGameView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     // Data
-    private var matrix = BooleanMatrix(1, 1)
-
-    fun setMatrix(newMatrix: BooleanMatrix?) {
-        val new = newMatrix ?: return
-        check(new.columnCount >= 1 && new.rowCount >= 1)
-        if (!(new sizeEquals matrix)) {
-            calculatePxSize(new, width, height)
+    fun setLives(lives: Set<Position>?) {
+        lives?.let {
+            pixels.clear()
+            var top: Float
+            var left: Float
+            for (cell in lives) {
+                left = pxSize * cell.x
+                top = pxSize * cell.y
+                pixels.add(RectF(left, top, left + pxSize, top + pxSize))
+            }
+            invalidate()
         }
-        matrix = new
-        calculatePixels()
-        invalidate()
     }
 
     // View
     private val paint = Paint()
     private val pixels = mutableListOf<RectF>()
     private var pxSize = 0f
+
+    var rows: Int = 0
+        set(value) {
+            field = value
+            calculatePxSize(width, height)
+            invalidate()
+        }
+
+    var columns: Int = 0
+        set(value) {
+            field = value
+            calculatePxSize(width, height)
+            invalidate()
+        }
 
     var liveColor: Int = 0
         set(value) {
@@ -46,39 +62,26 @@ class LifeGameView @JvmOverloads constructor(
     init {
         context.withStyledAttributes(attrs, R.styleable.LifeGameView) {
             liveColor = getColor(R.styleable.LifeGameView_liveColor, Color.GREEN)
+            rows = getInt(R.styleable.LifeGameView_rows, 1)
+            columns = getInt(R.styleable.LifeGameView_columns, 1)
         }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        calculatePxSize(matrix, w, h)
+        calculatePxSize(w, h)
     }
 
     override fun onDraw(canvas: Canvas?) {
-        if (canvas == null) {
-            return
-        }
-        val drawRect = { rect: RectF -> canvas.drawRect(rect, paint) }
-        pixels.forEach(drawRect)
-    }
-
-    private fun calculatePxSize(matrix: BooleanMatrix, w: Int, h: Int) {
-        val widthF = w.toFloat()
-        val heightF = h.toFloat()
-        pxSize = min(widthF / matrix.columnCount, heightF / matrix.rowCount)
-    }
-
-    private fun calculatePixels() {
-        pixels.clear()
-        var top: Float
-        var left: Float
-        for (x in matrix.columnIndices) {
-            left = pxSize * x
-            for (y in matrix.rowIndices) {
-                if (matrix[x, y]) {
-                    top = pxSize * y
-                    pixels.add(RectF(left, top, left + pxSize, top + pxSize))
-                }
+        canvas?.let { c ->
+            pixels.forEach { pixel ->
+                c.drawRect(pixel, paint)
             }
         }
+    }
+
+    private fun calculatePxSize(w: Int, h: Int) {
+        val widthF = w.toFloat()
+        val heightF = h.toFloat()
+        pxSize = min(widthF / columns, heightF / rows)
     }
 }
